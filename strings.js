a2aea94f5b2699b5be00af3161be292fc3275068
Sarah Morris <sarah.morris9@gmail.com>
2017-12-18 15:25:56 -0600

    finished exercise

--- strings.js
+++ strings.js
@@ -1,29 +1,47 @@
 let input = document.getElementById("input");
-let output = document.getElementById("output");
+let outputRev = document.getElementById("outputRev");
+let outputAlpha = document.getElementById("outputAlpha");
+let outputPal = document.getElementById("outputPal");
 let btn = document.getElementById("btn");
 let testString = "";
 
+// Event listeners
 
 btn.addEventListener("click", function () {
   testString = input.value;
-});
-
-function reversal(input) {
-  let newString = input.split("").reverse();  
-  console.log(newString);
-}
-  
-
-function alphabits() {
-  
-}
-  
-  
-function palindrome() {
-  
-}
-  
-
   reversal(testString);
   alphabits(testString);
   palindrome(testString);
+});
+
+input.addEventListener('keyup', function(event) {
+  if (event.which === 13) {
+    testString = input.value;
+    reversal(testString);
+    alphabits(testString);
+    palindrome(testString);
+  }
+});
+
+
+
+function reversal(input) {
+  let newString = input.split("").reverse();  
+  outputRev.innerHTML = newString.join("");
+}
+  
+
+function alphabits(input) {
+  let newerString = input.split("").sort();
+  outputAlpha.innerHTML = newerString.join("");
+}
+  
+  
+function palindrome(input) {
+  let newestString = input.split("").reverse().join("");
+  if (newestString === input) {
+    outputPal.innerHTML = "Your string is a palindrome!";
+  } else {
+    outputPal.innerHTML = "Whoops - no palindrome.";
+  }
+}
